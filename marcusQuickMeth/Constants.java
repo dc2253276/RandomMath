1e68076ddcf555e8d20536133ae27c7b16cfaf44
marcus8448 <marcus2475@outlook.com>
2018-02-13 21:17:01 -0800

    7.0

---
+++ marcusQuickMeth/Constants.java
@@ -0,0 +1,12 @@
+package marcusQuickMeth;
+
+public class Constants {
+
+	public static final int VERSIONBASE = 7;
+	public static final int VERSIONADDITION1 = 0;
+	public static final int VERSIONADDITION2 = 0;
+	protected static final String VERSIONNAMEFULL = "QuickMeth7.0.0a_RELEASE";
+	public static final String CurrentAuthor = "Marcus";
+	public static final boolean OUTOFDATE = false;
+	public static final boolean DEVVERSION = false;
+}
---
+++ marcusQuickMeth/Error.java
@@ -0,0 +1,46 @@
+package marcusQuickMeth;
+
+import javax.swing.JOptionPane;
+
+public class Error {
+	public static void Cancel() {
+		JOptionPane.showMessageDialog(null, "You cannot close the window / press cancel. To properly stop the program press '5'","Error", 2);
+		QuickMeth.start();
+	}
+	/**
+	 * For @Meth
+	 */
+	public static void NoEntry() {
+		JOptionPane.showMessageDialog(null, "You must type a number!","Error", 2);
+		QuickMeth.start();
+	}
+	/**
+	 * Now InvalidNumber();
+	 * Easier to use
+	 */
+	@Deprecated
+	public static void MaxSize() {
+		JOptionPane.showMessageDialog(null, "Your number is too long! It must be 10 characters and below (Including Negatives)","Error", 2);
+		QuickMeth.start();
+	}
+	/**
+	 * Now InvalidNumber();
+	 * Easier to use
+	 */
+	@Deprecated
+	public static void InvalidChars() {
+		JOptionPane.showMessageDialog(null, "Invalid characters in your answer","Error", 2);
+		QuickMeth.start();
+	}
+	public static void InvalidNumber() {
+		JOptionPane.showMessageDialog(null, "This number is invalid","Error", 2);
+	}
+	public static void Unknown() {
+		JOptionPane.showMessageDialog(null, "Unknown error, please contact the developer","Error", 2);
+		QuickMeth.start();
+	}
+	public static void MaxAmount() {
+		JOptionPane.showMessageDialog(null, "Your number is too big! It must be from 1-6!","Error", 2);
+		QuickMeth.start();
+	}
+}
