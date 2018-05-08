a9c14ec4b7543c497306ae902cf49be3a91a59ff
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2018-05-07 21:08:34 -0700

    Add files via upload

--- marcusQuickMeth/Constants.java
+++ marcusQuickMeth/Constants.java
@@ -1,12 +1,12 @@
-package marcusQuickMeth;
+package randomMath;
 
 public class Constants {
 
 	public static final int VERSIONBASE = 7;
 	public static final int VERSIONADDITION1 = 0;
 	public static final int VERSIONADDITION2 = 0;
-	protected static final String VERSIONNAMEFULL = "QuickMeth7.0.0a_RELEASE";
-	public static final String CurrentAuthor = "Marcus";
+	protected static final String VERSIONNAMEFULL = "RandomMath7.0.0a_RELEASE";
+	public static final String CurrentAuthor = "marcus8448";
 	public static final boolean OUTOFDATE = false;
 	public static final boolean DEVVERSION = false;
 }
