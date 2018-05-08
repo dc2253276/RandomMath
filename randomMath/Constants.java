6a51587f195099172a311ab40bce25478ef4ba5d
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2018-05-07 20:53:40 -0700

    Add files via upload

---
+++ randomMath/Constants.java
@@ -0,0 +1,10 @@
+package marcusQuickMeth;
+
+public class Constants {
+
+	public static final String VERSIONADDITION1 = "0";
+	public static final String VERSIONADDITION2 = "0";
+	public static final int VERSIONBASE = 5;
+	public static final String VERSIONNAMEFULL = "RandomMath5.0.0a_RELEASE";
+	public static final String CurrentAuthor = "marcus8448";
+}
